b8015960e1f137a1a4d40dd7d87a05daf9e5b91d
khanjawwadjk <53745446+khanjawwadjk@users.noreply.github.com>
2021-12-27 16:20:42 +0530

    final commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import {Card} from "react-bootstrap";
+import "./post.css";
+
+class Post extends Component{
+    render(){
+        return(
+            <div>
+                <h3>POST component</h3>
+                <div>
+                {this.props.list.map((vals, index)=>{ 
+                return(
+                        <Card id="myCard" style={{ width: '18rem' }} key={vals.id}>
+                        <Card.Img variant="top" src={vals.myImage} height="200px" width="200px" />
+                        <hr />
+                                <Card.Body>
+                                            <Card.Title><b>id:</b> {index + 1}</Card.Title>
+                                            <Card.Text><b>postName:</b> {vals.postName}</Card.Text>
+                                            <Card.Text><b>postDesc:</b> {vals.postDesc}</Card.Text>
+                                            <Card.Text><b>postAuthor:</b> {vals.author}</Card.Text>
+                                            <Card.Text><b>Date:</b> {vals.date}</Card.Text> 
+                            </Card.Body>
+                        </Card>
+                    )
+                })}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Post;
